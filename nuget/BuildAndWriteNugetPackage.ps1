7b0d3d900ff0acff636f7f78e9b71eaeaee1930b
Megan Adams <megan.adams@bonsai.ai>
2016-12-02 13:20:54 -0800

    Changes needed to compile with msvs-14

--- nuget/BuildAndWriteNugetPackage.ps1
+++ nuget/BuildAndWriteNugetPackage.ps1
@@ -19,26 +19,26 @@ $source_dir   = "$PSScriptRoot\.."
 
 # VS 2013
 #######################################################
-$build_dir = "./build/x64/v120/static"
-$generator = "Visual Studio 12 Win64"
+$build_dir = "./build/x64/v140/static"
+$generator = "Visual Studio 14 Win64"
 $options = @("-DBUILD_SHARED_LIBS=OFF","-DGTEST_FORCE_SHARED_CRT=ON")
 
 BuildPivot $source_dir $build_dir $generator $options
 
-$build_dir = "./build/x64/v120/dynamic"
-$generator = "Visual Studio 12 Win64"
+$build_dir = "./build/x64/v140/dynamic"
+$generator = "Visual Studio 14 Win64"
 $options = "-DBUILD_SHARED_LIBS=ON"
 
 BuildPivot $source_dir $build_dir $generator $options
 
-$build_dir = "./build/Win32/v120/static"
-$generator = "Visual Studio 12"
+$build_dir = "./build/Win32/v140/static"
+$generator = "Visual Studio 14"
 $options = @("-DBUILD_SHARED_LIBS=OFF","-DGTEST_FORCE_SHARED_CRT=ON")
 
 BuildPivot $source_dir $build_dir $generator $options
 
-$build_dir = "./build/Win32/v120/dynamic"
-$generator = "Visual Studio 12"
+$build_dir = "./build/Win32/v140/dynamic"
+$generator = "Visual Studio 14"
 $options ="-DBUILD_SHARED_LIBS=ON"
 
 BuildPivot $source_dir $build_dir $generator $options
